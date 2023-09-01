1f2968bb26697877654e1998ba9362aeb268db1b
abhinav <abhinav200218@gmail.com>
2023-09-01 20:51:45 +0530

    first commit

---
+++ model.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/google/uuid"
+	"learn/internal/database"
+	"time"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Email     string    `json:"email"`
+}
+
+func databaseUserToUser(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email:     dbUser.Email,
+	}
+
+}
